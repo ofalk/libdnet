f506215d9d89a690328fa4080dec9fdb32304203
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2001-10-11 05:40:04 +0000

    formatting

--- trunk/include/dnet/fw.h
+++ trunk/include/dnet/fw.h
@@ -14,17 +14,19 @@
 struct fw_rule {
 	char		device[14];	/* interface name */
 	u_char		op:4,		/* operation */
-#define FW_OP_ALLOW	1
-#define FW_OP_BLOCK	2
 			direction:4;	/* direction */
-#define FW_DIR_IN	1
-#define FW_DIR_OUT	2
 	u_char		proto;		/* IP protocol */
 	struct addr	src;		/* src address / net */
 	struct addr	dst;		/* dst address / net */
 	u_short		sport[2];	/* range or ICMP type / mask */
 	u_short		dport[2];	/* range or ICMP code / mask */
 };
+
+#define FW_OP_ALLOW	1
+#define FW_OP_BLOCK	2
+
+#define FW_DIR_IN	1
+#define FW_DIR_OUT	2
 
 typedef struct fw_handle fw_t;
 
