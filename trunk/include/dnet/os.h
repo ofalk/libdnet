46832f3115db1a3ffb1ac007c7d40ebce6494afd
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2001-10-12 06:49:48 +0000

    nasty catchall from BIND code

--- trunk/include/dnet/os.h
+++ trunk/include/dnet/os.h
@@ -52,8 +52,23 @@
 # define u_int8_t		uint8_t
 #endif
 
+/* Nastiness from old BIND code. */
 #ifndef DNET_BYTESEX
-# include "bytesex unknown"	/* XXX - HP-UX cpp lacks 'error' directive */
+# if defined(vax) || defined(ns32000) || defined(sun386) || defined(i386) || \
+    defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
+    defined(__alpha__) || defined(__alpha)
+#  define DNET_BYTESEX		DNET_LIL_ENDIAN
+# elif defined(sel) || defined(pyr) || defined(mc68000) || defined(sparc) || \
+    defined(is68k) || defined(tahoe) || defined(ibm032) || defined(ibm370) || \
+    defined(MIPSEB) || defined(_MIPSEB) || defined(_IBMR2) || defined(DGUX) ||\
+    defined(apollo) || defined(__convex__) || defined(_CRAY) || \
+    defined(__hppa) || defined(__hp9000) || \
+    defined(__hp9000s300) || defined(__hp9000s700) || \
+    defined (BIT_ZERO_ON_LEFT) || defined(m68k)
+#  define DNET_BYTESEX		DNET_BIG_ENDIAN
+# else
+#  error "bytesex unknown"
+# endif
 #endif
 
 #endif /* DNET_OS_H */
