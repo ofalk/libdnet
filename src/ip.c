195d5baddf5f4c2e1493371d641dae92fa1d7851
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2001-10-15 01:53:46 +0000

    fold ip-cooked into HAVE_COOKED_RAWIP, in ip.c

--- src/ip.c
+++ src/ip.c
@@ -24,8 +24,45 @@
 
 struct ip_handle {
 	int	fd;
+#ifdef HAVE_COOKED_RAWIP
+	eth_t		*eth;
+	intf_t		*intf;
+	arp_t		*arp;
+	route_t		*rt;
+	struct addr	 ip_src, ip_dst;
+	struct addr	 eth_src, eth_dst;
+#endif
 };
 
+#ifdef HAVE_COOKED_RAWIP
+ip_t *
+ip_open(void)
+{
+	ip_t *i;
+
+	if ((i = calloc(1, sizeof(*i))) == NULL)
+		return (NULL);
+	
+	if ((i->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		free(i);
+		return (NULL);
+	}
+	i->ip_src.addr_type = i->ip_dst.addr_type = ADDR_TYPE_IP;
+	i->ip_src.addr_bits = i->ip_dst.addr_bits = IP_ADDR_BITS;
+	
+	i->eth_src.addr_type = i->eth_dst.addr_type = ADDR_TYPE_ETH;
+	i->eth_src.addr_bits = i->eth_dst.addr_bits = ETH_ADDR_BITS;
+	
+	if ((i->intf = intf_open()) == NULL ||
+	    (i->arp = arp_open()) == NULL ||
+	    (i->rt = route_open()) == NULL) {
+		ip_close(i);
+		free(i);
+		return (NULL);
+	}
+	return (i);
+}
+#else /* !HAVE_COOKED_RAWIP */
 ip_t *
 ip_open(void)
 {
@@ -67,10 +104,99 @@ ip_open(void)
 		return (NULL);
 	}
 	i->fd = fd;
-
 	return (i);
 }
-		
+#endif /* !HAVE_COOKED_RAWIP */
+
+#ifdef HAVE_COOKED_RAWIP
+static int
+ip_match_intf(char *device, struct addr *addr, int flags, void *arg)
+{
+	ip_t *i = (ip_t *)arg;
+	
+	if (addr->addr_ip == i->ip_src.addr_ip ||
+	    i->ip_src.addr_ip == IP_ADDR_ANY) {
+		if (intf_get(i->intf, device, &i->eth_src, NULL) == 0) {
+			if (i->eth != NULL)
+				eth_close(i->eth);
+			if ((i->eth = eth_open(device)) == NULL)
+				return (-1);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+static int
+ip_lookup(ip_t *i, ip_addr_t dst)
+{
+	struct sockaddr_in sin;
+	struct addr gw;
+	int n;
+	
+	i->ip_dst.addr_ip = dst;
+
+	addr_ntos(&i->ip_dst, (struct sockaddr *)&sin);
+	sin.sin_port = htons(666);
+
+	/* XXX - Force the kernel to ARP for our destination. */
+	if (connect(i->fd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+		return (-1);	/* XXX */
+
+	/* Lookup our source address. */
+	n = sizeof(sin);
+	if (getsockname(i->fd, (struct sockaddr *)&sin, &n) < 0)
+		return (-1);	/* XXX */
+
+	addr_ston((struct sockaddr *)&sin, &i->ip_src);
+	
+	if (intf_loop(i->intf, ip_match_intf, i) != 1) {
+		i->ip_src.addr_ip = IP_ADDR_ANY;
+		if (intf_loop(i->intf, ip_match_intf, i) != 1)
+			return (-1);
+	}
+	/* Lookup our destination address. */
+	if (arp_get(i->arp, &i->ip_dst, &i->eth_dst) == 0)
+		return (0);
+	
+	if (route_get(i->rt, &i->ip_dst, &gw) == 0) {
+		if (gw.addr_ip != i->ip_src.addr_ip &&
+		    arp_get(i->arp, &gw, &i->eth_dst) == 0)
+			return (0);
+	}
+	memcpy(&i->eth_dst.addr_eth, ETH_ADDR_BROADCAST, ETH_ADDR_LEN);
+	
+	return (0);
+}
+
+ssize_t
+ip_send(ip_t *i, const void *buf, size_t len)
+{
+	struct ip_hdr *ip;
+	struct eth_hdr *eth;
+	u_char frame[ETH_LEN_MAX];
+	
+	ip = (struct ip_hdr *)buf;
+	
+	if (ip->ip_dst != i->ip_dst.addr_ip) {
+		if (ip_lookup(i, ip->ip_dst) < 0)
+			return (-1);
+	}
+	/* XXX - handle fragmentation here */
+
+	eth = (struct eth_hdr *)frame;
+	memcpy(&eth->eth_src, &i->eth_src.addr_eth, ETH_ADDR_LEN);
+	memcpy(&eth->eth_dst, &i->eth_dst.addr_eth, ETH_ADDR_LEN);
+	eth->eth_type = htons(ETH_TYPE_IP);
+	
+	memcpy(frame + ETH_HDR_LEN, buf, len);
+	
+	if (eth_send(i->eth, frame, ETH_HDR_LEN + len) != ETH_HDR_LEN + len)
+		return (-1);
+
+	return (len);
+}
+#else /* !HAVE_COOKED_RAWIP */
 ssize_t
 ip_send(ip_t *i, const void *buf, size_t len)
 {
@@ -86,7 +212,7 @@ ip_send(ip_t *i, const void *buf, size_t len)
 	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = ip->ip_dst;
 	
-#if defined(BSD) && !defined(OpenBSD)
+#if (defined(BSD) && !defined(OpenBSD)) || defined(UNIXWARE)
 	ip->ip_len = ntohs(ip->ip_len);
 	ip->ip_off = ntohs(ip->ip_off);
 
@@ -102,6 +228,7 @@ ip_send(ip_t *i, const void *buf, size_t len)
 	    (struct sockaddr *)&sin, sizeof(sin)));
 #endif
 }
+#endif /* !HAVE_COOKED_RAWIP */
 
 int
 ip_close(ip_t *i)
@@ -112,7 +239,16 @@ ip_close(ip_t *i)
 	}
 	if (close(i->fd) < 0)
 		return (-1);
-	
+#ifdef HAVE_COOKED_RAWIP	
+	if (i->intf != NULL)
+		intf_close(i->intf);
+
+	if (i->arp != NULL)
+		arp_close(i->arp);
+
+	if (i->rt != NULL)
+		route_close(i->rt);
+#endif
 	free(i);
 	return (0);
 }
