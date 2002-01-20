cae4fde66cdaaf601afc1549dd67e205537bba2c
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-01-20 18:59:21 +0000

    handle ip_off correctly

--- trunk/src/ip-util.c
+++ trunk/src/ip-util.c
@@ -107,7 +107,7 @@ void
 ip_checksum(void *buf, size_t len)
 {
 	struct ip_hdr *ip;
-	int hl, sum;
+	int hl, off, sum;
 
 	ip = (struct ip_hdr *)buf;
 	hl = ip->ip_hl << 2;
@@ -118,7 +118,8 @@ ip_checksum(void *buf, size_t len)
 	sum = ip_cksum_add(ip, hl, 0);
 	ip->ip_sum = ip_cksum_carry(sum);
 
-	if ((ip->ip_off & IP_OFFMASK) != 0)
+	off = htons(ip->ip_off);
+	if ((off & IP_OFFMASK) != 0 || (off & IP_MF) != 0)
 		return;
 	
 	if (ip->ip_p == IP_PROTO_TCP && len >= TCP_HDR_LEN) {
