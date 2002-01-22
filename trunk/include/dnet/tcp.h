79d7673b79fe888bfda62e61256d841d00a601d1
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-01-22 21:54:10 +0000

    add timestamp to opt union

--- trunk/include/dnet/tcp.h
+++ trunk/include/dnet/tcp.h
@@ -86,6 +86,7 @@ struct tcp_opt {
 		uint8_t		wscale[2];	/* XXX - scale + NOP */
 		uint16_t	sack[19];	/* XXX - origin / size pairs */
 		uint32_t	cc;
+		uint32_t	timestamp[2];
 		uint8_t		md5[16];
 		uint8_t		data8[TCP_OPT_LEN_MAX - TCP_OPT_LEN];
 	} opt_data;
