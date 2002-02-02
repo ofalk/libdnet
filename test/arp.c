df594d066f44875001788b2a6f0ff4506ac06c66
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-02-02 07:09:09 +0000

    update to new arp API

--- test/arp.c
+++ test/arp.c
@@ -28,17 +28,18 @@ usage(void)
 }
 
 static int
-print_arp(const struct addr *pa, const struct addr *ha, void *arg)
+print_arp(const struct arp_entry *entry, void *arg)
 {
-	printf("%s at %s\n", addr_ntoa(pa), addr_ntoa(ha));
+	printf("%s at %s\n", addr_ntoa(&entry->arp_pa),
+	    addr_ntoa(&entry->arp_ha));
 	return (0);
 }
 
 int
 main(int argc, char *argv[])
 {
-	struct addr pa, ha;
-	arp_t *a;
+	struct arp_entry entry;
+	arp_t *arp;
 	char *cmd;
 
 	if (argc < 2)
@@ -46,40 +47,35 @@ main(int argc, char *argv[])
 
 	cmd = argv[1];
 
-	if ((a = arp_open()) == NULL)
+	if ((arp = arp_open()) == NULL)
 		err(1, "arp_open");
 	
 	if (strcmp(cmd, "show") == 0) {
-		if (arp_loop(a, print_arp, NULL) < 0)
+		if (arp_loop(arp, print_arp, NULL) < 0)
 			err(1, "arp_loop");
 	} else if (strcmp(cmd, "get") == 0) {
-		if (addr_pton(argv[2], &pa) < 0)
+		if (addr_pton(argv[2], &entry.arp_pa) < 0)
 			err(1, "addr_pton");
-		
-		if (arp_get(a, &pa, &ha) < 0)
+		if (arp_get(arp, &entry) < 0)
 			err(1, "arp_get");
-
-		print_arp(&pa, &ha, NULL);
+		print_arp(&entry, NULL);
 	} else if (strcmp(cmd, "add") == 0) {
-		if (addr_pton(argv[2], &pa) < 0 || addr_pton(argv[3], &ha) < 0)
+		if (addr_pton(argv[2], &entry.arp_pa) < 0 ||
+		    addr_pton(argv[3], &entry.arp_ha) < 0)
 			err(1, "addr_pton");
-
-		if (arp_add(a, &pa, &ha) < 0)
+		if (arp_add(arp, &entry) < 0)
 			err(1, "arp_add");
-
-		printf("%s added\n", addr_ntoa(&pa));
+		printf("%s added\n", addr_ntoa(&entry.arp_pa));
 	} else if (strcmp(cmd, "delete") == 0) {
-		if (addr_pton(argv[2], &pa) < 0)
+		if (addr_pton(argv[2], &entry.arp_pa) < 0)
 			err(1, "addr_pton");
-
-		if (arp_delete(a, &pa) < 0)
+		if (arp_delete(arp, &entry) < 0)
 			err(1, "arp_delete");
-
-		printf("%s deleted\n", addr_ntoa(&pa));
+		printf("%s deleted\n", addr_ntoa(&entry.arp_pa));
 	} else
 		usage();
 
-	arp_close(a);
+	arp_close(arp);
 
 	exit(0);
 }
