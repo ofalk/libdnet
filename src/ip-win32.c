d5412307504ee75ecf4a213f16e486180f92edc9
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-03-29 01:39:26 +0000

    set better win32 last error

--- src/ip-win32.c
+++ src/ip-win32.c
@@ -40,9 +40,10 @@ ip_open(void)
 	
 	on = TRUE;
 	if (setsockopt(ip->fd, IPPROTO_IP, IP_HDRINCL,
-	    (const char *)&on, sizeof(on)) == SOCKET_ERROR)
+	    (const char *)&on, sizeof(on)) == SOCKET_ERROR) {
+		SetLastError(ERROR_NETWORK_ACCESS_DENIED);
 		return (ip_close(ip));
-	
+	}
 	ip->sin.sin_family = AF_INET;
 	ip->sin.sin_port = htons(666);
 	
