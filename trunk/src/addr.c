f77e412c7d1e825a3dc550e3925c50641f1549a3
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-12-02 04:10:40 +0000

    add IPv6 support, use our (faster) IPv4/IPv6 ntop/pton()

--- trunk/src/addr.c
+++ trunk/src/addr.c
@@ -35,63 +35,6 @@
 # define MAXHOSTNAMELEN	256
 #endif
 
-static const char *octet2dec[] = {
-	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-	"13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
-	"24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34",
-	"35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45",
-	"46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56",
-	"57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67",
-	"68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78",
-	"79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
-	"90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100",
-	"101", "102", "103", "104", "105", "106", "107", "108", "109",
-	"110", "111", "112", "113", "114", "115", "116", "117", "118",
-	"119", "120", "121", "122", "123", "124", "125", "126", "127",
-	"128", "129", "130", "131", "132", "133", "134", "135", "136",
-	"137", "138", "139", "140", "141", "142", "143", "144", "145",
-	"146", "147", "148", "149", "150", "151", "152", "153", "154",
-	"155", "156", "157", "158", "159", "160", "161", "162", "163",
-	"164", "165", "166", "167", "168", "169", "170", "171", "172",
-	"173", "174", "175", "176", "177", "178", "179", "180", "181",
-	"182", "183", "184", "185", "186", "187", "188", "189", "190",
-	"191", "192", "193", "194", "195", "196", "197", "198", "199",
-	"200", "201", "202", "203", "204", "205", "206", "207", "208",
-	"209", "210", "211", "212", "213", "214", "215", "216", "217",
-	"218", "219", "220", "221", "222", "223", "224", "225", "226",
-	"227", "228", "229", "230", "231", "232", "233", "234", "235",
-	"236", "237", "238", "239", "240", "241", "242", "243", "244",
-	"245", "246", "247", "248", "249", "250", "251", "252", "253",
-	"254", "255"
-};
-
-static const char *octet2hex[] = {
-	"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0a",
-	"0b", "0c", "0d", "0e", "0f", "10", "11", "12", "13", "14", "15",
-	"16", "17", "18", "19", "1a", "1b", "1c", "1d", "1e", "1f", "20",
-	"21", "22", "23", "24", "25", "26", "27", "28", "29", "2a", "2b",
-	"2c", "2d", "2e", "2f", "30", "31", "32", "33", "34", "35", "36",
-	"37", "38", "39", "3a", "3b", "3c", "3d", "3e", "3f", "40", "41",
-	"42", "43", "44", "45", "46", "47", "48", "49", "4a", "4b", "4c",
-	"4d", "4e", "4f", "50", "51", "52", "53", "54", "55", "56", "57",
-	"58", "59", "5a", "5b", "5c", "5d", "5e", "5f", "60", "61", "62",
-	"63", "64", "65", "66", "67", "68", "69", "6a", "6b", "6c", "6d",
-	"6e", "6f", "70", "71", "72", "73", "74", "75", "76", "77", "78",
-	"79", "7a", "7b", "7c", "7d", "7e", "7f", "80", "81", "82", "83",
-	"84", "85", "86", "87", "88", "89", "8a", "8b", "8c", "8d", "8e",
-	"8f", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
-	"9a", "9b", "9c", "9d", "9e", "9f", "a0", "a1", "a2", "a3", "a4",
-	"a5", "a6", "a7", "a8", "a9", "aa", "ab", "ac", "ad", "ae", "af",
-	"b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "ba",
-	"bb", "bc", "bd", "be", "bf", "c0", "c1", "c2", "c3", "c4", "c5",
-	"c6", "c7", "c8", "c9", "ca", "cb", "cc", "cd", "ce", "cf", "d0",
-	"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "da", "db",
-	"dc", "dd", "de", "df", "e0", "e1", "e2", "e3", "e4", "e5", "e6",
-	"e7", "e8", "e9", "ea", "eb", "ec", "ed", "ee", "ef", "f0", "f1",
-	"f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "fa", "fb", "fc",
-	"fd", "fe", "ff"
-};
-
 int
 addr_cmp(const struct addr *a, const struct addr *b)
 {
@@ -141,107 +84,74 @@ addr_bcast(const struct addr *a, struct addr *b)
 	return (0);
 }
 
-int
+char *
 addr_ntop(const struct addr *src, char *dst, size_t size)
 {
-	const char *p;
-	const u_char *u;
-	
-	u = src->addr_data8;
-	
 	if (src->addr_type == ADDR_TYPE_IP && size >= 20) {
-		for (p = octet2dec[u[0]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = '.';
-		for (p = octet2dec[u[1]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = '.';
-		for (p = octet2dec[u[2]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = '.';
-		for (p = octet2dec[u[3]]; (*dst = *p++) != '\0'; ) dst++;
-		
-		if (src->addr_bits < IP_ADDR_BITS) {
-			*dst++ = '/';
-			p = octet2dec[src->addr_bits];
-			while ((*dst = *p++) != '\0')
-				dst++;
+		if (ip_ntop(&src->addr_ip, dst, size) != NULL) {
+			if (src->addr_bits != IP_ADDR_BITS)
+				sprintf(dst + strlen(dst), "/%d",
+				    src->addr_bits);
+			return (dst);
 		}
-		return (0);
+	} else if (src->addr_type == ADDR_TYPE_IP6 && size >= 42) {
+		if (ip6_ntop(&src->addr_ip6, dst, size) != NULL) {
+			if (src->addr_bits != IP6_ADDR_BITS)
+				sprintf(dst + strlen(dst), "/%d",
+				    src->addr_bits);
+			return (dst);
+		}
 	} else if (src->addr_type == ADDR_TYPE_ETH && size >= 18) {
-		for (p = octet2hex[u[0]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = ':';
-		for (p = octet2hex[u[1]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = ':';
-		for (p = octet2hex[u[2]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = ':';
-		for (p = octet2hex[u[3]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = ':';
-		for (p = octet2hex[u[4]]; (*dst = *p++) != '\0'; ) dst++;
-		*dst++ = ':';
-		for (p = octet2hex[u[5]]; (*dst = *p++) != '\0'; ) dst++;
-
-		return (0);
+		if (src->addr_bits == ETH_ADDR_BITS)
+			return (eth_ntop(&src->addr_eth, dst, size));
 	}
 	errno = EINVAL;
-	
-	return (-1);
+	return (NULL);
 }
 
 int
 addr_pton(const char *src, struct addr *dst)
 {
-	char *p, tmp[MAXHOSTNAMELEN];
-	long l;
+	struct hostent *hp;
+	char *ep, tmp[300];
+	long bits = 0;
 	int i;
 	
-	if (strchr(src, ':') != NULL) {
+	for (i = 0; i < sizeof(tmp) - 1; i++) {
+		if (src[i] == '/') {
+			tmp[i] = '\0';
+			bits = strtol(&src[i + 1], &ep, 10);
+			if (ep == src || *ep != '\0' || bits < 0) {
+				errno = EINVAL;
+				return (-1);
+			}
+			break;
+		} else if ((tmp[i] = src[i]) == '\0')
+			break;
+	}
+	if (ip_pton(tmp, &dst->addr_ip) == 0) {
+		dst->addr_type = ADDR_TYPE_IP;
+		dst->addr_bits = IP_ADDR_BITS;
+	} else if (eth_pton(tmp, &dst->addr_eth) == 0) {
 		dst->addr_type = ADDR_TYPE_ETH;
 		dst->addr_bits = ETH_ADDR_BITS;
-		
-		for (i = 0; i < ETH_ADDR_LEN; i++) {
-			l = strtol(src, &p, 16);
-			if (p == src || l < 0 || l > 0xff) {
-				errno = EINVAL;
-				return (-1);
-			}
-			if (i < 5 && *p != ':') {
-				errno = EINVAL;
-				return (-1);
-			}
-			dst->addr_data8[i] = (u_char)l;
-			src = p + 1;
-		}
-		if (*p != '\0') {
+	} else if (ip6_pton(tmp, &dst->addr_ip6) == 0) {
+		dst->addr_type = ADDR_TYPE_IP6;
+		dst->addr_bits = IP6_ADDR_BITS;
+	} else if ((hp = gethostbyname(tmp)) != NULL) {
+		memcpy(&dst->addr_ip, hp->h_addr, IP_ADDR_LEN);
+		dst->addr_type = ADDR_TYPE_IP;
+		dst->addr_bits = IP_ADDR_BITS;
+	} else {
+		errno = EINVAL;
+		return (-1);
+	}
+	if (bits > 0) {
+		if (bits > dst->addr_bits) {
 			errno = EINVAL;
 			return (-1);
 		}
-	} else {
-		dst->addr_type = ADDR_TYPE_IP;
-		
-		if ((p = strchr(src, '/')) != NULL) {
-			if (++p - src > sizeof(tmp)) {
-				errno = EINVAL;
-				return (-1);
-			}
-			strlcpy(tmp, src, p - src);
-			src = p;
-			l = strtol(src, &p, 10);
-			if (p == src || l < 0 || l > IP_ADDR_BITS) {
-				errno = EINVAL;
-				return (-1);
-			}
-			dst->addr_bits = l;
-		} else {
-			strlcpy(tmp, src, sizeof(tmp));
-			dst->addr_bits = IP_ADDR_BITS;
-		}
-		if (inet_pton(AF_INET, tmp, &dst->addr_ip) != 1) {
-			struct hostent *hp;
-
-			if ((hp = gethostbyname(tmp)) == NULL) {
-				errno = EINVAL;
-				return (-1);
-			}
-			memcpy(&dst->addr_ip, hp->h_addr, IP_ADDR_LEN);
-		}
+		dst->addr_bits = (uint16_t)bits;
 	}
 	return (0);
 }
@@ -250,17 +160,15 @@ char *
 addr_ntoa(const struct addr *a)
 {
 	static char *p, buf[BUFSIZ];
-	char *q;
+	char *q = NULL;
 	
-	if (p == NULL || p > buf + sizeof(buf) - 32 /* XXX */)
+	if (p == NULL || p > buf + sizeof(buf) - 64 /* XXX */)
 		p = buf;
 	
-	if (addr_ntop(a, p, (buf + sizeof(buf)) - p) < 0)
-		return (NULL);
-
-	q = p;
-	p += strlen(p) + 1;
-	
+	if (addr_ntop(a, p, (buf + sizeof(buf)) - p) != NULL) {
+		q = p;
+		p += strlen(p) + 1;
+	}
 	return (q);
 }
 
