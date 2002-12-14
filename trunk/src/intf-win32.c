1a5b3646465583afa6e72223add077935746b5dc
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2002-12-14 04:01:37 +0000

    use our own Unix-y interface names and indices (e.g. eth0, lo0), mapped from Windows'.

--- trunk/src/intf-win32.c
+++ trunk/src/intf-win32.c
@@ -12,23 +12,43 @@
 #include <Iphlpapi.h>
 
 #include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "dnet.h"
 
+/* XXX - wonky */
+#define ETHIDXSZ	 24
+#define LOOPIDXSZ	 8
+#define IFTABLESZ	 4096
+#define IPTABLESZ	 1024
+
 struct intf_handle {
-	int	fd;
+	DWORD		 eth_idx[ETHIDXSZ];
+	int		 eth_cnt;
+	DWORD		 loop_idx[LOOPIDXSZ];
+	int		 loop_cnt;
+	MIB_IFTABLE	*iftable;
+	MIB_IPADDRTABLE	*iptable;
 };
 
 intf_t *
 intf_open(void)
 {
-	return ((intf_t *)calloc(1, sizeof(intf_t)));
+	intf_t *intf;
+
+	if ((intf = calloc(1, sizeof(*intf) + IFTABLESZ + IPTABLESZ)) != NULL){
+		intf->iftable = (MIB_IFTABLE *)((u_char *)intf +
+		    sizeof(*intf));
+		intf->iptable = (MIB_IPADDRTABLE *)((u_char *)intf->iftable +
+		    IFTABLESZ);
+	}
+	return (intf);
 }
 
 static int
-_intf_get_entry(const struct intf_entry *entry, void *arg)
+_match_intf_name(const struct intf_entry *entry, void *arg)
 {
 	struct intf_entry *e = (struct intf_entry *)arg;
 	
@@ -43,7 +63,7 @@ _intf_get_entry(const struct intf_entry *entry, void *arg)
 int
 intf_get(intf_t *intf, struct intf_entry *entry)
 {
-	if (intf_loop(intf, _intf_get_entry, entry) != 1) {
+	if (intf_loop(intf, _match_intf_name, entry) != 1) {
 		errno = ENXIO;
 		SetLastError(ERROR_NO_DATA);
 		return (-1);
@@ -66,9 +86,9 @@ _match_intf_src(const struct intf_entry *entry, void *arg)
 }
 
 int
-intf_get_src(intf_t *intf, struct intf_entry *entry, struct addr *dst)
+intf_get_src(intf_t *intf, struct intf_entry *entry, struct addr *src)
 {
-	memcpy(&entry->intf_addr, dst, sizeof(*dst));
+	memcpy(&entry->intf_addr, src, sizeof(*src));
 	
 	if (intf_loop(intf, _match_intf_src, entry) != 1) {
 		errno = ENXIO;
@@ -78,26 +98,32 @@ intf_get_src(intf_t *intf, struct intf_entry *entry, struct addr *dst)
 }
 
 static void
-_mibII_to_intf(MIB_IFROW *ifrow, MIB_IPADDRTABLE *iptable,
-    struct intf_entry *entry)
+_ifrow_to_entry(intf_t *intf, MIB_IFROW *ifrow, struct intf_entry *entry)
 {
 	struct addr *ap;
 	int i;
 	
 	memset(entry, 0, sizeof(*entry));
-	
-	strlcpy(entry->intf_name, ifrow->bDescr,
-	    sizeof(entry->intf_name));
-	
+
 	/* XXX - dwType matches MIB-II ifType. */
-	switch (ifrow->dwType) {
-	case MIB_IF_TYPE_ETHERNET:
-	case MIB_IF_TYPE_LOOPBACK:
+	if (ifrow->dwType == MIB_IF_TYPE_ETHERNET) {
+		for (i = 0; i < intf->eth_cnt; i++) {
+			if (intf->eth_idx[i] == ifrow->dwIndex)
+				break;
+		}
+		sprintf(entry->intf_name, "eth%d", i);
 		entry->intf_type = ifrow->dwType;
-		break;
-	default:
+	} else if (ifrow->dwType == MIB_IF_TYPE_LOOPBACK) {
+		for (i = 0; i < intf->loop_cnt; i++) {
+			if (intf->loop_idx[i] == ifrow->dwIndex)
+				break;
+		}
+		sprintf(entry->intf_name, "lo%d", i);
+		entry->intf_type = ifrow->dwType;
+	} else {
+		/* XXX */
+		sprintf(entry->intf_name, "nic%lu", ifrow->dwIndex);
 		entry->intf_type = INTF_TYPE_OTHER;
-		break;
 	}
 	/* Get interface flags. */
 	entry->intf_flags = 0;
@@ -122,34 +148,63 @@ _mibII_to_intf(MIB_IFROW *ifrow, MIB_IPADDRTABLE *iptable,
 	}
 	/* Get addresses. */
 	ap = entry->intf_alias_addrs;
-	for (i = 0; i < iptable->dwNumEntries; i++) {
-		if (iptable->table[i].dwIndex != ifrow->dwIndex)
-			continue;
-		
-		if (entry->intf_addr.addr_type != ADDR_TYPE_IP) {
-			entry->intf_addr.addr_type = ADDR_TYPE_IP;
-			entry->intf_addr.addr_ip = iptable->table[i].dwAddr;
-			addr_mtob(&iptable->table[i].dwMask, IP_ADDR_LEN,
-			    &entry->intf_addr.addr_bits);
-		} else {
-			ap->addr_type = ADDR_TYPE_IP;
-			ap->addr_ip = iptable->table[i].dwAddr;
-			addr_mtob(&iptable->table[i].dwMask, IP_ADDR_LEN,
-			    &ap->addr_bits);
-			ap++, entry->intf_alias_num++;
+	for (i = 0; i < intf->iptable->dwNumEntries; i++) {
+		if (intf->iptable->table[i].dwIndex == ifrow->dwIndex) {
+			if (entry->intf_addr.addr_type != ADDR_TYPE_IP) {
+				entry->intf_addr.addr_type = ADDR_TYPE_IP;
+				entry->intf_addr.addr_ip =
+				    intf->iptable->table[i].dwAddr;
+				addr_mtob(&intf->iptable->table[i].dwMask,
+				    IP_ADDR_LEN, &entry->intf_addr.addr_bits);
+			} else {
+				ap->addr_type = ADDR_TYPE_IP;
+				ap->addr_ip = intf->iptable->table[i].dwAddr;
+				addr_mtob(&intf->iptable->table[i].dwMask,
+				    IP_ADDR_LEN, &ap->addr_bits);
+				ap++, entry->intf_alias_num++;
+			}
 		}
 	}
 	entry->intf_len = (u_char *)ap - (u_char *)entry;
+}
+
+static int
+_refresh_tables(intf_t *intf)
+{
+	ULONG len;
+	int i;
+	
+        len = IFTABLESZ;
+	if (GetIfTable(intf->iftable, &len, FALSE) != NO_ERROR)
+		return (-1);
+
+	/* Map "unfriendly" win32 interface indices to ours. */
+	intf->eth_cnt = intf->loop_cnt = 0;
+	
+	for (i = 0; i < intf->iftable->dwNumEntries; i++) {
+		if (intf->iftable->table[i].dwType == MIB_IF_TYPE_ETHERNET &&
+		    intf->eth_cnt < ETHIDXSZ) {
+			intf->eth_idx[intf->eth_cnt++] =
+			    intf->iftable->table[i].dwIndex;
+		} else if (intf->iftable->table[i].dwType ==
+		    MIB_IF_TYPE_LOOPBACK && intf->loop_cnt < LOOPIDXSZ) {
+			intf->loop_idx[intf->loop_cnt++] =
+			    intf->iftable->table[i].dwIndex;
+		} else
+			return (-1);
+	}
+	len = IPTABLESZ;
+	if (GetIpAddrTable(intf->iptable, &len, FALSE) != NO_ERROR)
+		return (-1);
+	
+	return (0);
 }
 
 int
 intf_get_dst(intf_t *intf, struct intf_entry *entry, struct addr *dst)
 {
 	MIB_IFROW ifrow;
-	MIB_IPADDRTABLE *iptable;
-	u_char ipbuf[1024];
-	ULONG len;
-
+	
 	if (dst->addr_type != ADDR_TYPE_IP) {
 		errno = ENXIO;
 		SetLastError(ERROR_NO_DATA);
@@ -157,14 +212,14 @@ intf_get_dst(intf_t *intf, struct intf_entry *entry, struct addr *dst)
 	}
 	if (GetBestInterface(dst->addr_ip, &ifrow.dwIndex) != NO_ERROR)
 		return (-1);
-	
-        iptable = (MIB_IPADDRTABLE *)ipbuf;
-	len = sizeof(ipbuf);
-	
-	if (GetIpAddrTable(iptable, &len, FALSE) != NO_ERROR)
+
+	if (GetIfEntry(&ifrow) != NO_ERROR)
 		return (-1);
 	
-	_mibII_to_intf(&ifrow, iptable, entry);
+	if (_refresh_tables(intf) < 0)
+		return (-1);
+	
+	_ifrow_to_entry(intf, &ifrow, entry);
 	
 	return (0);
 }
@@ -175,6 +230,7 @@ intf_set(intf_t *intf, const struct intf_entry *entry)
 	/*
 	 * XXX - could set interface down via SetIfEntry(),
 	 * but what about the rest of the configuration? :-(
+	 * {Add,Delete}IPAddress for 2000/XP only
 	 */
 	errno = ENOSYS;
 	SetLastError(ERROR_NOT_SUPPORTED);
@@ -184,34 +240,22 @@ intf_set(intf_t *intf, const struct intf_entry *entry)
 int
 intf_loop(intf_t *intf, intf_handler callback, void *arg)
 {
-	MIB_IPADDRTABLE *iptable;
-	MIB_IFTABLE *iftable;
 	struct intf_entry *entry;
-	u_char ebuf[1024], ifbuf[4192], ipbuf[1024];
-	ULONG len;
-	int i, ret;
+	u_char ebuf[1024];
+	int i, ret = 0;
 
-	iftable = (MIB_IFTABLE *)ifbuf;
-	len = sizeof(ifbuf);
-
-	if (GetIfTable(iftable, &len, FALSE) != NO_ERROR)
+	if (_refresh_tables(intf) < 0)
 		return (-1);
-
-	iptable = (MIB_IPADDRTABLE *)ipbuf;
-	len = sizeof(ipbuf);
-
-	if (GetIpAddrTable(iptable, &len, FALSE) != NO_ERROR)
-		return (-1);
-
+	
 	entry = (struct intf_entry *)ebuf;
 	
-	for (i = 0; i < iftable->dwNumEntries; i++) {
-		_mibII_to_intf(&iftable->table[i], iptable, entry);
+	for (i = 0; i < intf->iftable->dwNumEntries; i++) {
+		_ifrow_to_entry(intf, &intf->iftable->table[i], entry);
 		
 		if ((ret = (*callback)(entry, arg)) != 0)
-			return (ret);
+			break;
 	}
-	return (0);
+	return (ret);
 }
 
 intf_t *
