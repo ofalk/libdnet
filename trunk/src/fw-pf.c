9f0b30f7a5da18e0630cc16787e386845ad31e5f
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2004-08-19 17:36:53 +0000

    update for OpenBSD-3.6. from Kirby Kuehl <kkuehl@gmail.com>

--- trunk/src/fw-pf.c
+++ trunk/src/fw-pf.c
@@ -29,8 +29,9 @@
 /*
  * XXX - cope with moving pf API
  */
-#if defined(DIOCRINABEGIN)
-/* OpenBSD 3.3 */
+#if defined(DIOCRINABEGIN) || defined(DIOCXBEGIN)
+/* OpenBSD 3.3 - 3.6 */
+/*     $OpenBSD: pfvar.h,v 1.197 2004/06/14 20:53:27 cedric Exp $ */
 /*     $OpenBSD: pfvar.h,v 1.130 2003/01/09 10:40:45 cedric Exp $ */
 /*     $OpenBSD: pfvar.h,v 1.127 2003/01/05 22:14:23 dhartmei Exp $ */
 # define PFRA_ADDR(ra)	(ra)->addr.v.a.addr.v4.s_addr
