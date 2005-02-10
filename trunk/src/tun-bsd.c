5ca4301bf96bdff35d7715e951fb5c5e9ab9b7de
dugsong <dugsong@0c474577-fa26-0410-a966-bdb198e94e9e>
2005-02-10 23:50:04 +0000

    whack tunnel header, support *BSD

--- trunk/src/tun-bsd.c
+++ trunk/src/tun-bsd.c
@@ -8,6 +8,10 @@
 
 #include "config.h"
 
+#include <sys/socket.h>
+#include <sys/uio.h>
+
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,6 +36,11 @@ tun_open(struct addr *src, struct addr *dst, int mtu)
 	char dev[128];
 	int i;
 
+	if (src->addr_type != ADDR_TYPE_IP || dst->addr_type != ADDR_TYPE_IP ||
+	    src->addr_bits != IP_ADDR_BITS || dst->addr_bits != IP_ADDR_BITS) {
+		errno = EINVAL;
+		return (NULL);
+	}
 	if ((tun = calloc(1, sizeof(*tun))) == NULL)
 		return (NULL);
 
@@ -48,14 +57,24 @@ tun_open(struct addr *src, struct addr *dst, int mtu)
 		
 		if ((tun->fd = open(dev, O_RDWR, 0)) != -1 &&
 		    intf_get(tun->intf, &tun->save) == 0) {
+			route_t *r;
+			struct route_entry entry;
+			
 			ifent.intf_flags = INTF_FLAG_UP|INTF_FLAG_POINTOPOINT;
-			// XXX - need to use dst netmask for interface netmask
 			ifent.intf_addr = *src;
 			ifent.intf_dst_addr = *dst;	
 			ifent.intf_mtu = mtu;
 			
 			if (intf_set(tun->intf, &ifent) < 0)
 				tun = tun_close(tun);
+
+			/* XXX - try to ensure our route got set */
+			if ((r = route_open()) != NULL) {
+				entry.route_dst = *dst;
+				entry.route_gw = *src;
+				route_add(r, &entry);
+				route_close(r);
+			}
 			break;
 		}
 	}
@@ -79,13 +98,37 @@ tun_fileno(tun_t *tun)
 ssize_t
 tun_send(tun_t *tun, const void *buf, size_t size)
 {
+#ifdef __OpenBSD__
+	struct iovec iov[2];
+	uint32_t af = htonl(AF_INET);
+
+	iov[0].iov_base = &af;
+	iov[0].iov_len = sizeof(af);
+	iov[1].iov_base = (void *)buf;
+	iov[1].iov_len = size;
+	
+	return (writev(tun->fd, iov, 2));
+#else
 	return (write(tun->fd, buf, size));
+#endif
 }
 
 ssize_t
 tun_recv(tun_t *tun, void *buf, size_t size)
 {
+#ifdef __OpenBSD__
+	struct iovec iov[2];
+	uint32_t af;
+	
+	iov[0].iov_base = &af;
+	iov[0].iov_len = sizeof(af);
+	iov[1].iov_base = (void *)buf;
+	iov[1].iov_len = size;
+	
+	return (readv(tun->fd, iov, 2) - sizeof(af));
+#else
 	return (read(tun->fd, buf, size));
+#endif
 }
 
 tun_t *
@@ -94,6 +137,7 @@ tun_close(tun_t *tun)
 	if (tun->fd > 0)
 		close(tun->fd);
 	if (tun->intf != NULL) {
+		/* Restore interface configuration on close. */
 		intf_set(tun->intf, &tun->save);
 		intf_close(tun->intf);
 	}
