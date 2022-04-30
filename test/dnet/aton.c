6e4e82f81e4244c709d5eef368b9ca93dbab10f5
Oliver Falk <oliver@linux-kernel.at>
2022-04-30 15:36:35 +0200

    Fix and close #17

--- test/dnet/aton.c
+++ test/dnet/aton.c
@@ -86,7 +86,7 @@ off_aton(char *string, uint16_t *off)
 	} else {
 		i = strtol(string, &p, 10);
 		if (*string == '\0' || (*p != '\0' && *p != '+') ||
-		    i > IP_OFFMASK)
+		    i > (IP_OFFMASK << 3))
 			return (-1);
 		*off = htons(((*p == '+') ? IP_MF : 0) | (i >> 3));
 	}
